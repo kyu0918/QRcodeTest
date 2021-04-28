f6dd928fd678ce09322cc860443674f1ffd8a374
kyu0918 <58536057+kyu0918@users.noreply.github.com>
2021-04-28 20:54:39 +0900

    QR코드 스캔 세로버젼으로 업데이트

--- app/src/main/java/com/example/myqrcodetest/ScanQR.java
+++ app/src/main/java/com/example/myqrcodetest/ScanQR.java
@@ -10,13 +10,17 @@ import com.google.zxing.integration.android.IntentResult;
 
 
 public class ScanQR extends AppCompatActivity {
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_q_r);
 
-        new IntentIntegrator(this).initiateScan();
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false); // default가 세로모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+        qrScan.setPrompt("QR코드를 선에 맞춰 스캔해주세요.");
+        qrScan.initiateScan();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
